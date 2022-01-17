1a51f79bb4d11363cce411b8410eef6849f45c26
Aniketw1551 <aniketwaghmare976@gmail.com>
2022-01-17 20:46:52 +0530

    Adding MovePositon program

--- SnakeLadderGame/StartPosition.cs
+++ SnakeLadderGame/StartPosition.cs
@@ -10,13 +10,40 @@ namespace SnakeLadderGame
     {
         // Instance variable
         int position = 0;
+        const int MaxPosition = 100;
+        const int MinPosition = -1;
+        const int LADDER = 1;
+        const int SNAKE = 2;
         Random random = new Random(); //Calling object class  
 
         public void StartGame() //Calling Method
         {
             Console.WriteLine("Player position is: " + position);
-            int RandomNum = random.Next(1,7); //Generating random number between 0 and 6
-            Console.WriteLine("randomNum: " + randomNum);
+            int i = 0;
+            while (i <= MaxPosition) //Using while loop till game reaches max position
+
+                int DiceValue = random.Next(1, 7); //Generating random number between 0 and 6
+            int PlayCheck = random.Next(0, 3); //Generating random number between 0 and 2 to control movement of player position
+            if (position <= MinPosition)
+            {
+                position = 0;
+            }
+            else
+            {
+                switch (PlayCheck)
+                case (LADDER)
+                position = += DiceValue;
+                    break
+                case (SNAKE)
+                position = -= DiceValue;
+                    break;
+                default;
+                    break;
+                }
+            }
+            i++;
+        }
+            Console.WriteLine("Position of player after end of game: " + position);
         }
     }
 }
