2073f74c364608d95adfe5459242f957b8dbe296
Aniketw1551 <aniketwaghmare976@gmail.com>
2022-01-17 20:11:40 +0530

    Adding StartPosition program

---
+++ SnakeLadderGame/StartPosition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadderGame
+{
+    public class StartPosition
+    {
+        // Intance variable
+        int position = 0;
+
+        public void StartGame() //Calling Method
+        {
+            Console.WriteLine("Player position is: " + position);
+        }
+    }
+}
