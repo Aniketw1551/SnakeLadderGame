819bb66bb69fffd499ee76e95db5a9a8da0b7af4
Aniketw1551 <aniketwaghmare976@gmail.com>
2022-01-17 20:25:10 +0530

    Adding RollDice program

--- SnakeLadderGame/StartPosition.cs
+++ SnakeLadderGame/StartPosition.cs
@@ -8,12 +8,15 @@ namespace SnakeLadderGame
 {
     public class StartPosition
     {
-        // Intance variable
+        // Instance variable
         int position = 0;
+        Random random = new Random(); //Calling object class  
 
         public void StartGame() //Calling Method
         {
             Console.WriteLine("Player position is: " + position);
+            int RandomNum = random.Next(1,7); //Generating random number between 0 and 6
+            Console.WriteLine("randomNum: " + randomNum);
         }
     }
 }
